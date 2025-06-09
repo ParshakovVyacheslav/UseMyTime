449c9bf348c83d359750be366db4fd9d462b24de
ParshakovVyacheslav <parsakovslava03@gmail.com>
2025-06-09 13:17:33 +0700

    Add main page

--- UseMyTime/UseMyTime/settings.py
+++ UseMyTime/UseMyTime/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'UseMyTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- UseMyTime/contacts/urls.py
+++ UseMyTime/contacts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/', views.contacts, name='contacts'),
+    path('', views.contacts, name='contacts'),
     path('ask/', views.ask, name='ask')
 ]
