607a36acdcd7c56f6a5d931748552f6fb250b223
ParshakovVyacheslav <parsakovslava03@gmail.com>
2025-06-30 20:06:25 +0700

    Change DB to SQLite for tests

--- UseMyTime/UseMyTime/settings.py
+++ UseMyTime/UseMyTime/settings.py
@@ -54,10 +54,8 @@ WSGI_APPLICATION = 'UseMyTime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'usemytime',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
